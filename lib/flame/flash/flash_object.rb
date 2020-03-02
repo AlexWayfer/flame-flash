c39faf08a6f2dcc476d20d11002c491f9ed58c1c
Alexander Popov <alex.wayfer@gmail.com>
2020-03-03 02:22:28 +0300

    Improve documentation of methods

--- lib/flame/flash/flash_object.rb
+++ lib/flame/flash/flash_object.rb
@@ -9,6 +9,7 @@ module Flame
 		class FlashObject
 			attr_reader :now, :next
 
+			## Initialize Flash Object with specific session, parent and scope
 			def initialize(session, parent = nil, scope = nil)
 				@now = FlashArray.new(session.to_a)
 				@next = FlashArray.new
@@ -16,16 +17,19 @@ module Flame
 				@scope = scope
 			end
 
+			## Return Flash Object at given scope
 			def scope(key = nil)
 				return self unless key
 
 				self.class.new(now.select(scope: key), self, key)
 			end
 
+			## Add entry with type and text
 			def []=(type, text)
 				@parent.next.push type, text, scope: (@scope if @parent != self)
 			end
 
+			## Get entries by type
 			def [](type = nil)
 				now.select(type: type).map { |hash| hash[:text] }
 			end
